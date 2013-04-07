b8d161438328819b9b9b3ddd242b6196ebacbf42
Eric Song <regonics@gmail.com>
2013-04-06 22:29:43 -0400

    added static serve

--- hackny/hackny/urls.py
+++ hackny/hackny/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib import admin
+import os
 
+admin.autodiscover()
 
 urlpatterns = patterns('',
      url(r'^$', 'home.views.homepage'),
      url(r'^home/', 'home.views.homepage'),
+)
 
+urlpatterns += patterns('',
+    (r'^static/(?<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }),
 )
